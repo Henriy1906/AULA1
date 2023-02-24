8f5ec8866a24273c4369b516c01d966bc554860e
Henriy1906 <henriquedeitosdias@gmail.com>
2023-02-24 11:09:56 -0300

    Testes com twig

--- produtos.php
+++ produtos.php
@@ -8,6 +8,27 @@ $twig = new \Twig\Environment($loader);
 
 $template = $twig-> load('produtos.html');
 
+$produtos = [
+    [
+        'nome' => 'Chinelo',
+        'preco' => 30,
+    ],
+    [
+        'nome' => 'Camiseta',
+        'preco' => 50,
+    ],
+    [
+        'nome' => 'Boné',
+        'preco' => 39.9
+    ],
+    [
+        'nome' => 'Carro',
+        'preco' => 35000
+    ],
+];
+
 echo $template->render([
-    'título' => 'Produtos'
+    'título' => 'Produtos',
+    //'produtos' => $produtos,
+    'produtos' => null,
 ]);
