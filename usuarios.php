d7c3282c821b844d33c23200132305ce1ae1d653
Henriy1906 <henriquedeitosdias@gmail.com>
2023-03-30 10:10:18 -0300

    models

--- usuarios.php
+++ usuarios.php
@@ -2,10 +2,11 @@
 
 require('verifica_login.php');
 require('twig_carregar.php');
-require('pdo.inc.php');
 
-$sql = $pdo->query('SELECT id, username, admin, ativo FROM usuarios WHERE ativo = 1');
-$usuarios = $sql->fetchAll(PDO::FETCH_ASSOC);
+require('models/Model.php');
+require('models/Usuario.php');
+
+$usr = new Usuario();
 
 echo $twig->render('usuarios.html', [
     'usuarios' => $usuarios
